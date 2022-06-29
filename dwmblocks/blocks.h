d27544a35bec20dedb5f9d9c3ef68255248bb830
Mike <mike@techknowledg.ie>
2022-06-29 14:04:39 +0100

    added dwmblocks

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",        " battery.sh",	50,          		0},
+	{"",        " date.sh",		3600,          		0},
+	{"",        " time.sh ",	60,          		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
---
+++ scripts/time.sh
@@ -0,0 +1,1 @@
+/home/mike/bin/scripts/time.sh
